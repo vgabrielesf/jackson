577583c6f83fef1ed1d101bf2a6bf5927efa00a4
Vitoria Gabriele da Silva Figueiredo <vitoriagabriele@inace.local>
2026-02-19 12:35:39 -0300

    atualização

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Home from './pages/Home';
 import { ThemeProvider } from './hooks/useTheme';
+import { renderingImages } from './data/services';
+
+function preloadImages(imageList) {
+  imageList.forEach(img => {
+    const image = new window.Image();
+    image.src = img.src;
+  });
+}
 
 const App = () => {
+  useEffect(() => {
+    preloadImages(renderingImages);
+  }, []);
+
   return (
     <ThemeProvider>
       <Home />
